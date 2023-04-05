b745579df2e045ff1658e218c4b333867db6d8f8
Raul <rauldronk@gmail.com>
2023-04-05 20:33:04 +0300

    Initial Commit

---
+++ SmoothDropdown/SmoothDropdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmoothDropdownApp.swift
+//  SmoothDropdown
+//
+//  Created by Raul on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmoothDropdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
